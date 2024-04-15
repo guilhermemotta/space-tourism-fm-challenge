db6bc4663b1d7d18b6e33c04127564aa86793cb8
guilhermemotta <g.a.f.motta@gmail.com>
2024-04-15 18:10:10 -0300

    feature: inicio da implementação desktop da página Crew

--- src/routes/crew.tsx
+++ src/routes/crew.tsx
@@ -22,33 +22,35 @@ function Crew() {
         meet your crew
       </Heading>
 
-      <section className="mx-auto flex flex-col items-center gap-8 px-6 py-8 md:max-w-xl md:gap-10 md:py-16 md:pb-0 lg:flex-row">
-        <div className="h-[223px] w-full md:bottom-0 md:order-last md:h-auto md:w-[456px]">
+      <section className="mx-auto flex flex-col items-center gap-8 px-6 py-8 md:max-w-xl md:gap-10 md:py-16 md:pb-0 lg:flex-row xl:mx-0 xl:max-w-max xl:gap-0 xl:p-0">
+        <div className="h-[223px] w-full md:bottom-0 md:order-last md:h-auto md:w-[456px] xl:flex-auto xl:self-end">
           <img
             className="mx-auto h-[223px] md:h-auto md:w-[456px]"
             src={imageUrl}
             alt={`${crewData[currentTabIndex].name}'s picture`}
           />
+
           <hr className="my-0 w-full border-t-[1px] border-[#383B4B] md:hidden" />
         </div>
 
-        <div className="z-10 flex flex-row gap-4">
+        <div className="z-10 flex flex-row gap-4 xl:fixed xl:bottom-24 xl:gap-6">
           {crewData.map((_, index) => (
             <span
               key={index}
               className={`${
                 currentTabIndex === index ? "opacity-100" : "opacity-[17.44%]"
-              } h-[10px] w-[10px] cursor-pointer rounded-full bg-white`}
+              } h-[10px] w-[10px] cursor-pointer rounded-full bg-white transition-opacity hover:opacity-50 xl:h-[15px] xl:w-[15px]`}
               onClick={() => setCurrentTabIndex(index)}
             ></span>
           ))}
         </div>
 
-        <section className="text-center md:order-first">
-          <div className="font-serif text-[16px] uppercase text-gray md:text-[24px]">
+        <section className="text-center md:order-first xl:text-start">
+          <div className="font-serif text-[16px] uppercase text-gray opacity-50 md:text-[24px] xl:text-[32px]">
             {crewData[currentTabIndex].role}
           </div>
-          <div className="mb-4 font-serif text-[24px] uppercase text-white md:text-[40px]">
+
+          <div className="mb-4 font-serif text-[24px] uppercase text-white md:text-[40px] xl:text-[56px]">
             {crewData[currentTabIndex].name}
           </div>
 
