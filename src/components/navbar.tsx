ca592afd8d2cafa3847b08b155db6f55027b82d2
guilhermemotta <g.a.f.motta@gmail.com>
2024-04-18 10:28:11 -0300

    feature(navbar): Adição do traço à esquerda da barra de navegação

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,10 @@
 import { Link, useLocation } from "react-router-dom";
 
+import NavButton from "./navbutton";
+
 import logo from "../assets/shared/logo.svg";
 import iconHamburger from "../assets/shared/icon-hamburger.svg";
 import iconClose from "../assets/shared/icon-close.svg";
-import NavButton from "./navbutton";
 
 const pages = [
   { index: "00", route: "home" },
@@ -16,10 +17,12 @@ function Navbar() {
   const location = useLocation();
 
   return (
-    <header className="z-50 flex w-full flex-row justify-between ps-6 md:mb-10 md:ps-9 lg:pt-[40px] xl:ps-14">
+    <header className="z-50 flex w-full flex-row justify-between ps-6 md:pb-10 md:ps-9 lg:pt-[40px] xl:ps-14">
       <Link to="/" className="py-6">
         <img className="h-[40px] w-[40px] md:h-[48px] md:w-[48px]" src={logo} />
       </Link>
+      {/* width: 30rem */}
+      <hr className="absolute left-[10rem] z-[51] my-8 hidden w-4/12 border-t-[1px] border-white border-opacity-25 text-start md:mb-6 md:mt-12 xl:block" />
 
       <div className="md:w-[60%]">
         {/* <div className="md:min-w-[450px]"> */}
@@ -28,6 +31,7 @@ function Navbar() {
           id="drawer-toggle"
           className="peer fixed h-0 w-0 appearance-none opacity-0"
         />
+
         <label
           htmlFor="drawer-toggle"
           className="inline-flex appearance-none px-6 py-8 md:hidden"
